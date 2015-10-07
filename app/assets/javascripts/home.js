4fc3195a73ca5904d9bc2f08d7aead8d31a4fcb3
reddingk <kris.redding3@gmail.com>
2015-10-07 20:10:35 +0000

    Add Release section to home page

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -14,49 +14,68 @@ $(document).ready(function() {
     //Kick off the initial slide animation when the document is ready
     $(".transition-timer-carousel-progress-bar", "#transition-timer-carousel")
         .css("width", "100%");
-});
 
-$(document).ready(function() { 
-/* Demo Scripts for Bootstrap Carousel and Animate.css article
-* on SitePoint by Maria Antonietta Perna
-*/
-(function( $ ) {
-
-	//Function to animate slider captions 
-	function doAnimations( elems ) {
-		//Cache the animationend event in a variable
-		var animEndEv = 'webkitAnimationEnd animationend';
-		
-		elems.each(function () {
-			var $this = $(this),
-				$animationType = $this.data('animation');
-			$this.addClass($animationType).one(animEndEv, function () {
-				$this.removeClass($animationType);
-			});
-		});
-	}
-	
-	//Variables on page load 
-	var $myCarousel = $('.carousel-animate'),
-		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
-		
-	//Initialize carousel 
-	$myCarousel.carousel();
-	
-	//Animate captions in first slide on page load 
-	doAnimations($firstAnimatingElems);
-	
-	//Pause carousel  
-	$myCarousel.carousel('pause');
-	
-	
-	//Other slides to be animated on carousel slide event 
-	$myCarousel.on('slide.bs.carousel', function (e) {
-		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
-		doAnimations($animatingElems);
-	});  
-	
-})(jQuery);
-
+    /*Animation*/
+    (function( $ ) {
     
+    	//Function to animate slider captions 
+    	function doAnimations( elems ) {
+    		//Cache the animationend event in a variable
+    		var animEndEv = 'webkitAnimationEnd animationend';
+    		
+    		elems.each(function () {
+    			var $this = $(this),
+    				$animationType = $this.data('animation');
+    			$this.addClass($animationType).one(animEndEv, function () {
+    				$this.removeClass($animationType);
+    			});
+    		});
+    	}
+    	
+    	//Variables on page load 
+    	var $myCarousel = $('.carousel-animate'),
+    		$firstAnimatingElems = $myCarousel.find('.item:first').find("[data-animation ^= 'animated']");
+    		
+    	//Initialize carousel 
+    	$myCarousel.carousel();
+    	
+    	//Animate captions in first slide on page load 
+    	doAnimations($firstAnimatingElems);
+    	
+    	//Pause carousel  
+    	$myCarousel.carousel('pause');
+    	
+    	
+    	//Other slides to be animated on carousel slide event 
+    	$myCarousel.on('slide.bs.carousel', function (e) {
+    		var $animatingElems = $(e.relatedTarget).find("[data-animation ^= 'animated']");
+    		doAnimations($animatingElems);
+    	});  
+    	
+    })(jQuery);
+    
+    /*Release*/
+    $('#releaseCarousel').carousel({
+    	interval: 0
+	});
+	
+	var clickEvent = false;
+	$('#releaseCarousel').on('click', '.nav a', function() {
+			clickEvent = true;
+			$('.nav li').removeClass('active');
+			$(this).parent().addClass('active');		
+	}).on('slid.bs.carousel', function(e) {
+		if(!clickEvent) {
+			var count = $('.nav').children().length -1;
+			var current = $('.nav li.active');
+			current.removeClass('active').next().addClass('active');
+			var id = parseInt(current.data('slide-to'));
+			if(count == id) {
+				$('.nav li').first().addClass('active');	
+			}
+		}
+		clickEvent = false;
+	});
+
 });
+
