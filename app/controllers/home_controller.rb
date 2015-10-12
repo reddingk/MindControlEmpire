0c8315f0002db7002c1c40b547b019da61d59a0b
reddingk <kris.redding3@gmail.com>
2015-10-12 20:29:53 +0000

    Added Images & Home page updates

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -22,6 +22,25 @@ class HomeController < ApplicationController
   		  end
   		end
   	end
+  	## Events
+  	@newevents = []
+  	@jdata["events"].each do |event|
+  	  if @newevents.size < 7 && event["date"].to_date > (Date.today - 1.day)
+  	    @jdata["artists"].each do |artist|
+  	      if event["artistname"] == artist["name"]
+  	        event["photo"] = artist["image"]
+  	      end
+  	    end
+  	    if event["photo"].nil? || event["photo"].empty?
+  	      event["photo"] = "_MCE_logo.jpg"
+  	    end
+  	    if event["artistname"].nil? || event["artistname"].empty? 
+  	      event["artistname"] = "Mind Control Empire"
+  	    end
+  	    @newevents << event
+  	  end
+  	end
+  	@newevents.sort_by!{|e| e["date"].to_date }
   	
   	## 
   end
