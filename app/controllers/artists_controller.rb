6aee78dd064209fb2b26a777f50a3e446d68cd12
reddingk <kris.redding3@gmail.com>
2015-10-13 20:46:13 +0000

    Add Artist individual Page

---
+++ app/controllers/artists_controller.rb
@@ -0,0 +1,32 @@
+class ArtistsController < ApplicationController
+  def index
+    @jfile = File.read(Rails.public_path+"info.json")	
+  	@jdata = JSON.parse(@jfile)
+  	
+  	@artists = []
+  	@jdata["artists"].each do |artist|
+  	  if artist["image"].nil? || artist["image"].empty?
+  	    artist["image"] = "_MCE_logo.jpg"
+  	  end
+  	  @artists << artist
+  	end
+  	
+  end
+
+  def individual
+    @artistname = params[:artist]
+    
+    @jfile = File.read(Rails.public_path+"info.json")	
+  	@jdata = JSON.parse(@jfile)
+    
+  	@jdata["artists"].each do |artist|
+  	  if artist["name"] == @artistname
+  	    if artist["image"].nil? || artist["image"].empty?
+  	      artist["image"] = "_MCE_logo.jpg"
+  	    end
+  	    @talent = artist
+  	  end
+  	end
+  	
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
 
   
+# Artists
+  get 'artists/index'
+  get 'Artists/Empire', to: 'artists#individual'
+  #get 'artists/individual'
 
 # CMS - Editor  
   get 'cm_s/index'
