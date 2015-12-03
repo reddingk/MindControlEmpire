ae31b8c270d210bfcb3fb251ba90bc1670e68256
Kris Redding <kris.redding3@gmail.com>
2015-12-03 01:39:46 +0000

    New database updates

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -4,24 +4,30 @@ class EventsController < ApplicationController
   	@jdata = JSON.parse(@jfile)
     
     @comingevents = []
+    @pastevents = []
+    
   	@jdata["events"].each do |event|
+  	  @jdata["artists"].each do |artist|
+  	    if event["artistname"] == artist["name"]
+  	      event["photo"] = artist["image"]
+  	    end
+  	  end
+  	  if event["photo"].nil? || event["photo"].empty?
+  	    event["photo"] = "_MCE_logo.jpg"
+  	  end
+  	  if event["artistname"].nil? || event["artistname"].empty? 
+  	    event["artistname"] = "Mind Control Empire"
+  	  end
+  	  ## Add to coming events or Past Events
   	  if event["date"].to_date > (Date.today - 1.day)
-  	    @jdata["artists"].each do |artist|
-  	      if event["artistname"] == artist["name"]
-  	        event["photo"] = artist["image"]
-  	      end
-  	    end
-  	    if event["photo"].nil? || event["photo"].empty?
-  	      event["photo"] = "_MCE_logo.jpg"
-  	    end
-  	    if event["artistname"].nil? || event["artistname"].empty? 
-  	      event["artistname"] = "Mind Control Empire"
-  	    end
   	    @comingevents << event
+  	  else
+  	    @pastevents << event
   	  end
   	end
   	@comingevents.sort_by!{|e| e["date"].to_date }
-  
+    @pastevents.sort_by!{|e| e["date"].to_date }.reverse!
+    
   ##End index
   end
 end
