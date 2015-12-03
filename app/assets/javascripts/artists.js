4e00feea877f3e6186f662744d123fe3075d1001
Kris Redding <kris.redding3@gmail.com>
2015-12-03 18:41:52 +0000

    Remove Site music from individual pages

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -12,19 +12,6 @@ $(document).on('click','.release-pages .mce-pagination li a', function() {
         $('.artist-releases').html(data);
     }, "html");
     
-    // AJAX
-    /*$.ajax({
-      url: "/artists/get_artistReleases",
-      type: "get", //send it through get method
-      data:{page: $(this).data("id") , a_name: $(this).data("artist")},
-      success: function(response) {
-        $('.artist-releases').html(response);
-       
-      },
-      error: function(xhr) {
-        console.log(xhr);
-      }
-    });*/
 });
 
 $( document ).ready(function() {
