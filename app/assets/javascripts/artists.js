6a284365d07fe613a29dbb64c13e2a747eb33e16
Kris Redding <kris.redding3@gmail.com>
2015-12-03 17:47:26 +0000

    Page Test

--- app/assets/javascripts/artists.js
+++ app/assets/javascripts/artists.js
@@ -7,14 +7,29 @@ $(document).on('click','.release-pages .mce-pagination li a', function() {
     $('#pagination'+ $(this).data("id")).addClass("active");
     
     console.log("1 - [p]: " + $(this).data("id") + ", [a]: "+$(this).data("artist"));
-    //$.get('/Artists/get_artistReleases', $.param({ page: $(this).data("id"), a_name: $(this).data("artist") }), function(data){ 
-     $.get('../artists/get_artistReleases', $.param({ page: $(this).data("id"), a_name: $(this).data("artist") }), function(data){ 
+    
+    // GET
+    /*$.get('/artists/get_artistReleases', $.param({ page: $(this).data("id"), a_name: $(this).data("artist") }), function(data){ 
         
         console.log("2 - [p]: " + $(this).data("id") + ", [a]: "+$(this).data("artist"));
-        
         $('.artist-releases').html(data);
-    }, "html");
+    }, "html");*/
     
+    // AJAX
+    $.ajax({
+      url: "/artists/get_artistReleases",
+      type: "get", //send it through get method
+      data:{page: $(this).data("id") , a_name: $(this).data("artist")},
+      success: function(response) {
+        $('.artist-releases').html(response);
+        console.log("WORKED !!!");
+      },
+      error: function(xhr) {
+        console.log("2 - [p]: " + $(this).data("id") + ", [a]: "+$(this).data("artist"));
+        console.log("DID NOT WORK");
+        console.log(xhr);
+      }
+    });
 });
 
 $( document ).ready(function() {
