fa371af6ea77de8d4e3ac187f3d66bcd81ac58dc
Kris Redding <kris.redding3@gmail.com>
2016-10-16 20:49:04 -0400

    artist page updates

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -221,6 +221,41 @@
 })();
 
 (function(){
+   "use strict";
+
+    angular.module('directives').directive('sectionSelect', ['$window', function($window) {
+      return {
+        restrict: 'EA',
+        link: function ($scope, element, attrs) {
+
+          angular.element($window).bind("scroll", function() {
+
+            var selectOffset = 200;
+
+            var windowp = angular.element($window)[0];
+            var topThreshHold = element[0].offsetTop - selectOffset;
+            var bottomThreshHold = (element[0].offsetTop + element[0].offsetHeight) - selectOffset;
+
+            if(windowp.pageYOffset >= topThreshHold && windowp.pageYOffset < bottomThreshHold){
+              if(!element.hasClass("screenSelect")){
+                element.addClass("screenSelect");
+              }
+            }
+            else {
+              if(element.hasClass("screenSelect")){
+                element.removeClass("screenSelect");
+              }
+            }
+
+          });
+        }
+      }
+
+    }]);
+
+})();
+
+(function(){
  "use strict";
 
   angular.module('artistsCtrl').controller('ArtistsController', ['$state','$stateParams','mceInfo','$sce', function($state, $stateParams, mceInfo, $sce){
@@ -335,41 +370,6 @@
       }
       function swapSelected(item) {
         vm.spotSelected = item;
-      }
-
-    }]);
-
-})();
-
-(function(){
-   "use strict";
-
-    angular.module('directives').directive('sectionSelect', ['$window', function($window) {
-      return {
-        restrict: 'EA',
-        link: function ($scope, element, attrs) {
-
-          angular.element($window).bind("scroll", function() {
-
-            var selectOffset = 200;
-
-            var windowp = angular.element($window)[0];
-            var topThreshHold = element[0].offsetTop - selectOffset;
-            var bottomThreshHold = (element[0].offsetTop + element[0].offsetHeight) - selectOffset;
-
-            if(windowp.pageYOffset >= topThreshHold && windowp.pageYOffset < bottomThreshHold){
-              if(!element.hasClass("screenSelect")){
-                element.addClass("screenSelect");
-              }
-            }
-            else {
-              if(element.hasClass("screenSelect")){
-                element.removeClass("screenSelect");
-              }
-            }
-
-          });
-        }
       }
 
     }]);
