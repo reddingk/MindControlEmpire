f97c2f5bd62e4eac85f7c1bb287a4622878168da
Kris Redding <kris.redding3@gmail.com>
2016-10-22 18:09:39 -0400

    home page mobile updates

--- public/js/bundle.js
+++ public/js/bundle.js
@@ -716,10 +716,13 @@
 (function(){
  "use strict";
 
-  angular.module('releasesCtrl').controller('ReleasesController', ['$state', 'mceInfo',function($state,mceInfo){
+  angular.module('releasesCtrl').controller('ReleasesController', ['$state', 'mceInfo', '$sce',function($state,mceInfo,$sce){
     var vm = this;
     vm.releasesList = mceInfo.releases.all();
     vm.allReleases = {"videos":[], "mixtapes":[], "tracks":[]};
+    vm.backSVG = "views/templates/_gravity.html";
+
+    var itemDisplayMax = 3;
 
     for(var i =0; i < vm.releasesList.length; i++)
     {
@@ -740,6 +743,44 @@
       "mixtapes":{"page":1, "totalpages":vm.allReleases.mixtapes.length , "releases":vm.allReleases.mixtapes[0]},
       "profiles":{"page":1, "totalpages":Math.ceil(vm.allReleases.tracks.length / itemDisplayMax), "releases":vm.allReleases.tracks.slice(0,3)},
     };
+
+    /*Functions*/
+    vm.releasePaging = releasePaging;
+    vm.checkButton = checkButton;
+    vm.URLClean = URLClean;
+
+    function URLClean(url) {
+      return $sce.trustAsResourceUrl(url);
+    }
+
+    function checkButton(direction, type) {
+      var bool = false;
+
+      if(direction == "up"){
+        if((vm.displayedReleases[type].page + 1) <= vm.displayedReleases[type].totalpages) {bool = true;}
+      }
+      else{
+        if((vm.displayedReleases[type].page - 1) > 0) { bool = true;}
+      }
+      return bool;
+    }
+
+    function releasePaging(direction, type){
+      if(direction == "up"){
+        if((vm.displayedReleases[type].page + 1) <= vm.displayedReleases[type].totalpages)
+        {
+          vm.displayedReleases[type].page +=1;
+        }
+      }
+      else{
+        if((vm.displayedReleases[type].page - 1) > 0)
+        {
+          vm.displayedReleases[type].page -=1;
+        }
+      }
+      var first = itemDisplayMax * (vm.displayedReleases[type].page  - 1);
+      vm.displayedReleases[type].releases = (type != "mixtapes" ? vm.allReleases[type].slice(first,first+ itemDisplayMax) : vm.allReleases[type][(vm.displayedReleases[type].page  - 1)]);
+    }
 
   }]);
 
